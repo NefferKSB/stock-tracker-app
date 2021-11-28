329aabf382de6189dc0df6044ddfd3b0540603d3
NefferKSB <kennanrsb@gmail.com>
2021-11-27 22:23:50 -0500

    Added getStockData call in the StockData Service

--- src/app/Services/Stock-Data/stock-data.service.ts
+++ src/app/Services/Stock-Data/stock-data.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockDataService {
 
-  constructor() { }
+  private endpoint = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=uLLH_N__TQuHqpD9LWYZEa_rUG4u2tG8';
+
+  constructor(private http: HttpClient) { }
+
+  getStockData(): Observable<any> {
+    return this.http.get(this.endpoint);
+    /*
+    return this.http.get(endpoint).pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+    */
+  }
+
+  private extractData(res: Response): any {
+    const body = res;
+    return body || { };
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 }
