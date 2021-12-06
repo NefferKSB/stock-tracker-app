7a15ef604c2ece383c7aec2e631134a418cb3fde
NefferKSB <kennanrsb@gmail.com>
2021-12-05 23:17:26 -0500

    Added Stock Info Card Updates

--- src/app/Components/Stock-Card/stock-info-card.component.ts
+++ src/app/Components/Stock-Card/stock-info-card.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StockDataService } from 'src/app/Services/Stock-Data/stock-data.service';
+import { TickerDetails } from './ticker-details';
 
 @Component({
   selector: 'stock-info-card',
   templateUrl: './stock-info-card.component.html',
   styleUrls: ['./stock-info-card.component.css']
 })
-export class StockInfoCardComponent {
+export class StockInfoCardComponent implements OnInit {
   value = 'Clear me';
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
+  tickerInfo: any = {};
+  dailyTickerValue: any = {};
 
   constructor(private api: StockDataService) { }
 
-  getTickerInfo() {
-    this.api.getTickerInfo('MSFT')
+  ngOnInit() {}
+
+  getTickerInfo(tickerSymbol: String) {
+    this.api.getTickerInfo(tickerSymbol)
     .subscribe(response => {
-      console.log(JSON.stringify(response));
+      this.tickerInfo = response.results[0];
+      this.getDailyStockValue(tickerSymbol);
+      console.log(JSON.stringify(this.tickerInfo));
+    })
+
+  }
+
+  getDailyStockValue(tickerSymbol: String) {
+    this.api.getStockOpenCloseData(tickerSymbol)
+    .subscribe(response => {
+      this.dailyTickerValue = response;
     })
   }
 }
--- src/app/Services/Stock-Data/stock-data.service.ts
+++ src/app/Services/Stock-Data/stock-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StockOpenCloseDetails } from 'src/app/Components/Stock-Table/stock-open-close-details';
+import { TickerDetails } from 'src/app/Components/Stock-Card/ticker-details';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,7 @@ export class StockDataService {
   }
 
   getTickerInfo(symbol: String): Observable<any> {
-    //https://api.polygon.io/v3/reference/tickers?ticker=KO&active=true&sort=ticker&order=asc&limit=1&apiKey=uLLH_N__TQuHqpD9LWYZEa_rUG4u2tG8
     this.endpoint = 'https://api.polygon.io/v3/reference/tickers?ticker=' + symbol + '&active=true&sort=ticker&order=asc&limit=1&apiKey=uLLH_N__TQuHqpD9LWYZEa_rUG4u2tG8';
-    return this.http.get<StockOpenCloseDetails>(this.endpoint)
+    return this.http.get<TickerDetails>(this.endpoint)
   }
 }
